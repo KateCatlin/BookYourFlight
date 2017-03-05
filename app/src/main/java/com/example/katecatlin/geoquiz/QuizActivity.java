2e54d1b940b702093c3c073d9953796f816d6ff8
Kate Catlin <kcatlin2@gmail.com>
2017-03-05 00:06:01 -0700

    Initial setup

---
+++ app/src/main/java/com/example/katecatlin/geoquiz/QuizActivity.java
@@ -0,0 +1,47 @@
+package com.example.katecatlin.geoquiz;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button TrueButton;
+    private Button FalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        TrueButton = (Button)findViewById(R.id.true_button);
+        TrueButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        FalseButton = (Button)findViewById(R.id.false_button);
+        FalseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+}
