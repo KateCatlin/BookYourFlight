86f5ab42f9afc248269a0932ecd646e8ede6e2ac
Kate Catlin <kcatlin2@gmail.com>
2017-03-31 08:21:23 -0600

    fixing toasts

--- app/src/main/java/com/example/katecatlin/geoquiz/MoreMotivation.java
+++ app/src/main/java/com/example/katecatlin/geoquiz/MoreMotivation.java
@@ -1,20 +1,49 @@
 package com.example.katecatlin.geoquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MoreMotivation extends AppCompatActivity {
+    public static final String EXTRA_ANSWER_TRUE = "com.example.katecatlin.geoquiz.answer_is_true";
+    public static final String EXTRA_PIC_SHOWN = "com.example.katecatlin.geoquiz.pic_shown";
+
+    private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowPic;
+
+    private void setAnswerShownPic(boolean isPicShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_PIC_SHOWN, isPicShown);
+        setResult(RESULT_OK, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_motivation);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setAnswerShownPic(false);
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_TRUE, false);
+
+        mAnswerTextView = (TextView)findViewById(R.id.answerTextView);
+
+        mShowPic = (Button)findViewById(R.id.showPicButton);
+        mShowPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAnswerTextView.setText("(By the way, the answer is " + mAnswerIsTrue + ")");
+                setAnswerShownPic(true);
+            }
+        });
+
     }
 
 }
--- app/src/main/java/com/example/katecatlin/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/katecatlin/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.example.katecatlin.geoquiz;
 
+import android.content.Intent;
 import android.media.Image;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -20,17 +21,16 @@ import com.google.android.gms.common.api.GoogleApiClient;
 public class QuizActivity extends AppCompatActivity {
     private static final String TAG = "QuizActivity";
 
-    private Button TrueButton;
-    private Button FalseButton;
-    private ImageButton NextButton;
-    private ImageButton PreviousButton;
+    private Button TrueButton, FalseButton, MotivationButton;
+    private ImageButton NextButton, PreviousButton;
     private TextView QuestionTextView;
+    private boolean mIsWeak;
 
     private TrueFalse[] QuestionBank = new TrueFalse[]{
             new TrueFalse(R.string.question_GCC, true),
             new TrueFalse(R.string.question_weather, false),
             new TrueFalse(R.string.question_free, true),
-            new TrueFalse(R.string.question_flights, true),d
+            new TrueFalse(R.string.question_flights, true),
     };
 
     private int CurrentIndex = 0;
@@ -41,17 +41,18 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void checkAnswer(boolean userPressedTrue) {
-
         boolean answerIsTrue = QuestionBank[CurrentIndex].isTrueQuestion();
-
         int messageResId = 0;
-
-        if (userPressedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
+        
+        if (mIsWeak) {
+            messageResId = R.string.judgement_toast;
         } else {
-            messageResId = R.string.incorrect_toast;
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
         }
-
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 
@@ -93,12 +94,32 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 CurrentIndex = (CurrentIndex + 1) % QuestionBank.length;
+                mIsWeak = false;
                 updateQuestion();
             }
 
         });
 
+        MotivationButton = (Button)findViewById(R.id.motivation_button);
+        MotivationButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(QuizActivity.this, MoreMotivation.class);
+                boolean answerIsTrue = QuestionBank[CurrentIndex].isTrueQuestion();
+                i.putExtra(MoreMotivation.EXTRA_ANSWER_TRUE, answerIsTrue);
+                startActivityForResult(i, 0);
+            }
+        });
+
         updateQuestion();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {
+            return;
+        }
+        mIsWeak = data.getBooleanExtra(MoreMotivation.EXTRA_PIC_SHOWN, false);
     }
 
 }
