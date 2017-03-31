a253f45be69534d5c55a015ed84e0ee6e8b5efc4
Kate Catlin <kcatlin2@gmail.com>
2017-03-31 07:22:18 -0600

    Getting rid of auto-generated junk

--- app/src/main/java/com/example/katecatlin/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/katecatlin/geoquiz/QuizActivity.java
@@ -30,7 +30,7 @@ public class QuizActivity extends AppCompatActivity {
             new TrueFalse(R.string.question_GCC, true),
             new TrueFalse(R.string.question_weather, false),
             new TrueFalse(R.string.question_free, true),
-            new TrueFalse(R.string.question_flights, true),
+            new TrueFalse(R.string.question_flights, true),d
     };
 
     private int CurrentIndex = 0;
